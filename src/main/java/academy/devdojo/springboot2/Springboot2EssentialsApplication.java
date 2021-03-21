9afb85feff88339c97af4a72548974fd7f16f77b
ceduardo <edunogodz@gmail.com>
2021-03-21 15:08:30 -0300

    Connect to Database

--- src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
+++ src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot2EssentialsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Springboot2EssentialsApplication.class, args);
 	}
 
--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,23 +1,29 @@
 package academy.devdojo.springboot2.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Anime {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @SequenceGenerator(
+            name="anime_sequence",
+            sequenceName="anime_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "anime_sequence"
+    )
+    private Long id;
     private String name;
-
-    public Anime(String name) {
-        this.name = name;
-    }
-
-    public Anime() {
-    }
-
-    //erro: No serializer found for class academy.devdojo.springboot2.domain.Anime
-    //pq nao tinha os getter and setters
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
